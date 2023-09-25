7887425297f085c0b20800a6df84cd9f12fb6e21
lgsus <jisusdeivid@gmail.com>
2023-09-24 19:46:22 -0500

    mejora de footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import imageBanner from "../../public/images/image1.jpg";
 import image2 from "../../public/images/image2.png";
+import image3 from "../../public/images/image3.jpg";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-white">
-      <Image
-        src={image2}
-        alt="Your Image"
-        objectFit="cover"
-        objectPosition="center top"
-      />
+      <div className="relative min-h-screen h-1/2">
+        <Image
+          src={image2}
+          style={{ objectFit: "cover"}}
+          alt="Your Image"
+        />
+      </div>
       <div className="absolute top-96 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center text-center text-white">
         <h1 className="text-7xl font-bold">CREA TU PROPIO DISEÑO</h1>
         <p className="text-4xl mt-4">el límite es tu imaginación</p>
@@ -135,20 +137,27 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-gray-900 text-white py-8">
-        <div className="max-w-7xl mx-auto px-4">
-          <div className="flex flex-col items-center text-center">
-            {/* Información de contacto */}
+      <div className="relative">
+        <Image
+          src="/images/image3.jpg"
+          alt="Your Image"
+          style={{ objectFit: "cover" }}
+          height={100}
+          width={1320}
+        />
+        <div className="absolute left-1/2 bottom-2 -translate-x-1/2  text-center">
+          <div className="flex flex-col items-center">
             <div>
-              <h3 className="text-3xl font-semibold mb-2">Contáctanos</h3>{" "}
-              {/* Aumenta el tamaño de la letra */}
-              <p className="text-xl">Número de contacto: +123456789</p>{" "}
-              {/* Aumenta el tamaño de la letra */}
+              <h3 className="text-7xl font-bold mb-36">¡Contáctanos!</h3>
+              {/* Ajusta el margen superior de los párrafos */}
+              <p className="text-4xl font-semibold">Email:</p>
+              <p className="text-xl mb-24">correo@correo.com</p>
+              <p className="text-4xl font-semibold">Número de contacto:</p>
+              <p className="text-xl mb-22">+123456789</p>
+              {/* Ajusta el margen superior de los párrafos */}
             </div>
             {/* Iconos de redes sociales */}
-            <div className="flex space-x-4 mt-4">
-              {" "}
-              {/* Agrega espacio arriba para separar el número de los iconos */}
+            <div className="flex space-x-8 mt-20 mb-10">
               <a href="#" className="text-white">
                 <FaFacebook size={24} />
               </a>
